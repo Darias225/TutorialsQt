4429feb2b67679d6b3c57845d792e9ecca293cf0
Dani Arias <darias@1eds.local>
2024-02-28 11:29:29 +0100

    Struct
    Herencia, variables Staticas

--- test120240227/main.cpp
+++ test120240227/main.cpp
@@ -1,31 +1,114 @@
 #include <QCoreApplication>
 #include <QDebug>
 
-enum enumArray{
-    NORMAL,
-    INVERSO,
+struct Pos2D
+{
+protected:
+    double x;
+    double y;
+
+
+    Pos2D() {
+        this->x = 0;
+        this->y = 0;
+    }
+    Pos2D(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    void printPos(){
+        qDebug() <<
+            "Pos X : " << x << "\n"
+            "Pos Y : " << y << "\n";
+    }
+    ~Pos2D(){
+    }
+
 };
 
-void printArray(int v[], int tam, enumArray forma){
-    int *p = v;
-    if(forma == NORMAL){
-        for(int i=0; i < tam; i++){
-            qDebug() << *(p+i);
+struct Nacionalidad {
+
+
+protected:
+    QString name;
+
+    Nacionalidad(){
+        this->name = "";
+    }
+
+    Nacionalidad(QString name){
+        this->name = name;
+
+    }
+    ~Nacionalidad(){
+    }
+
+    void printNacion(){
+        qDebug() << "nacion : " << name;
+    }
+};
+
+struct Persona : Pos2D, Nacionalidad {
+
+private:
+    static int nPerson;
+    int nPersonAct;
+
+public:
+
+    int edad;
+
+    Persona()
+    : Pos2D(), Nacionalidad()
+    {
+        edad = 0;
+        nPersonAct = 0;
+        if(nPersonAct>=0)
+        {
+            nPerson ++;
+            nPersonAct = nPerson;
         }
-    }else if(forma == INVERSO) {
-        for(int i=tam-1; i >= 0 ; i--){
-            qDebug() << *(p+i);
+        }
+
+    Persona(int edad, int x, int y,QString name)
+    :  Pos2D(x, y), Nacionalidad(name)
+    {
+        this->edad = edad;
+        nPersonAct = 0;
+        if(nPersonAct>=0)
+        {
+            nPerson ++;
+            nPersonAct = nPerson;
         }
     }
-}
+
+    int getNPersona(){
+        return this->nPerson;
+    }
+
+    void printPersona(){
+        printPos();
+        qDebug()
+            << "Edad" << edad <<"\n";
+        printNacion();
+        qDebug()
+            << "Num persona" << nPersonAct;
+    }
+
+};
+
+int Persona::nPerson = 0;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    int v[] ={1,2,3,4,5};
+    Persona p1 = Persona(26,0,0,"ff");
+    Persona p2 = Persona(30,0,0,"ff");
 
-    printArray(v,5,INVERSO);
+    p1.printPersona();
+    p2.printPersona();
 
     return a.exec();
 }
