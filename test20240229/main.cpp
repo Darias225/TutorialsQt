396ae1fdfb9fff29761285329d1de69302cedc29
1EDS\darias <darias225@gmail.com>
2024-02-29 14:41:20 +0100

    Operators +-*/

--- test20240229/main.cpp
+++ test20240229/main.cpp
@@ -1,45 +1,52 @@
 #include <QCoreApplication>
 
-struct Punto2D
+class Punto2D
 {
+public:
     double x,y;
+    Punto2D() {
+        x = 0;
+        y = 0;
+    }
+    Punto2D(double x, double y) {
+        this->x = x;
+        this->y = y;
+    }
+    ~Punto2D(){
+        qDebug() << "Deleted point ( " << x <<", " << y << " )";
+    }
 
-    void inicializa(double, double);
-    Punto2D suma(const Punto2D &);
-    void printPunto();
+    void printPoint(){
+        qDebug() << "( " << x <<", " << y << " )";
+    }
+
 };
 
-void Punto2D::inicializa(double x, double y){
-    this->x = x;
-    this->y = y;
+Punto2D operator+(const Punto2D & p1, const Punto2D & p2){
+    return Punto2D(p1.x+p2.y,p1.y+p2.y);
 }
-
-Punto2D Punto2D::suma(const Punto2D &p){
-    Punto2D pAux;
-
-    pAux.x = x + p.x;
-    pAux.y = y + p.y;
-
-   return pAux;
+Punto2D operator-(Punto2D p1, Punto2D p2){
+    return Punto2D(p1.x-p2.y,p1.y-p2.y);
 }
-
-void Punto2D::printPunto(){
-    qDebug() << x;
-    qDebug() << y;
+Punto2D operator*(Punto2D p1, Punto2D p2){
+    return Punto2D(p1.x*p2.y,p1.y*p2.y);
+}
+Punto2D operator/(const Punto2D &p1,const Punto2D & p2){
+    return Punto2D(p1.x/p2.y,p1.y/p2.y);
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Punto2D p1,p2,p3;
+    Punto2D *p1 = new Punto2D(1,2);
+    Punto2D *p2 = new Punto2D(3,4);
 
-    p1.inicializa(1,1);
-    p2.inicializa(2,2);
-    p3.inicializa(3,3);
 
-    p1.suma(p3).printPunto();
+    *p1+*p2;
 
+    delete p1;
+    delete p2;
 
     return a.exec();
 }
